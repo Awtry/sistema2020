082e9058053d9ad6a7a0d4c24a1197d48aa68336
Awtry <jorge_verah@outlook.es>
2022-03-24 19:56:36 -0600

    Ajuste carpetas y  practica 3 segundo parcial

---
+++ SegundoParcial/Practica3-2P-Fetch3/back.js
@@ -0,0 +1,50 @@
+
+var coordenadas = {
+    lat: 0,
+    lng: 0,
+}
+
+var propiedades = {
+    center: coordenadas,
+    zoom: 2,
+}
+
+function inciarMapa() {
+    const map = new google.maps.Map(document.getElementById("map"), propiedades);
+
+    const marcadores = obtieneMarcadores();
+
+    for(marcador of marcadores) {
+
+      let marker = new google.maps.Marker({
+        map: map,
+        position: new google.maps.LatLng(marcador.latitude, marcador.longitude),
+        title: marcador.name
+      })
+    }
+
+
+  }
+
+function obtieneMarcadores() {
+
+  const markers = [
+    {
+      name : "México",
+      longitude : "-99.1276",
+      latitude : "19.427"
+    },
+    {
+      name : "Brasil",
+      longitude : "-47.9292",
+      latitude : "40.4167"
+    },
+    {
+      name : "España",
+      longitude : "-3.70327",
+      latitude : "40.4167"
+    }
+  ];
+
+  return markers
+}
