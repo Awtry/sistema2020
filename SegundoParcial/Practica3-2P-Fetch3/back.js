eb49232a069fadbe62b9a43321960980d89c97a8
Awtry <jorge_verah@outlook.es>
2022-04-02 17:32:04 -0600

    Formato y credencial google maps

--- SegundoParcial/Practica3-2P-Fetch3/back.js
+++ SegundoParcial/Practica3-2P-Fetch3/back.js
@@ -1,50 +1,45 @@
-
 var coordenadas = {
-    lat: 0,
-    lng: 0,
-}
+  lat: 0,
+  lng: 0,
+};
 
 var propiedades = {
-    center: coordenadas,
-    zoom: 2,
-}
+  center: coordenadas,
+  zoom: 2,
+};
 
 function iniciaMapa() {
-    const map = new google.maps.Map(document.getElementById("map"), propiedades);
+  const map = new google.maps.Map(document.getElementById("map"), propiedades);
 
-    const marcadores = obtieneMarcadores();
+  const marcadores = obtieneMarcadores();
 
-    for(marcador of marcadores) {
-
-      let marker = new google.maps.Marker({
-        map: map,
-        position: new google.maps.LatLng(marcador.latitude, marcador.longitude),
-        title: marcador.name
-      })
-    }
-
-
+  for (marcador of marcadores) {
+    let marker = new google.maps.Marker({
+      map: map,
+      position: new google.maps.LatLng(marcador.latitude, marcador.longitude),
+      title: marcador.name,
+    });
   }
+}
 
 function obtieneMarcadores() {
-
   const markers = [
     {
-      name : "México",
-      longitude : "-99.1276",
-      latitude : "19.427"
+      name: "México",
+      longitude: "-99.1276",
+      latitude: "19.427",
     },
     {
-      name : "Brasil",
-      longitude : "-47.9292",
-      latitude : "40.4167"
+      name: "Brasil",
+      longitude: "-47.9292",
+      latitude: "40.4167",
     },
     {
-      name : "España",
-      longitude : "-3.70327",
-      latitude : "40.4167"
-    }
+      name: "España",
+      longitude: "-3.70327",
+      latitude: "40.4167",
+    },
   ];
 
-  return markers
+  return markers;
 }
--- SegundoParcial/Practica4-2P/back.js
+++ SegundoParcial/Practica4-2P/back.js
@@ -1,17 +1,17 @@
 var map;
 
 var coordenadas = {
-    lat: 0,
-    lng: 0,
+  lat: 0,
+  lng: 0,
 };
 
 var propiedades = {
-    center: coordenadas,
-    zoom: 20,
+  center: coordenadas,
+  zoom: 20,
 };
 
 function iniciaMapa() {
-    /*
+  /*
     var icono = {
         url: "https://media.giphy.com/media/2NJkigfPiu2dmpZg99/giphy.gif",
         scaledSize: new google.maps.Size(50, 50),
@@ -20,32 +20,29 @@ function iniciaMapa() {
     };
     */
 
-    fetch('https://sistemasgeo.com/2022/fetch/paises.json')
-        .then(response => {
-            mode: no-cors
-            response.json().then(datos => {
-                const map = new google.maps.Map(document.getElementById("map"), propiedades);
-                console.log(datos);
-                datos.forEach(marcador => {
-                    console.log(marcador);
-                    var informacion = "<strong>País: </strong>" + marcador.CountryName;
-                    var infoWindow = new google.maps.infoWindow({
-                        content: informacion
-                    });
-                    let marker = new google.maps.Marker({
-                        map: map,
-                        position: new google.maps.LatLng(marcador.CapitalLatitude, marcador.CapitalLongitude),
-                        title: marcador.CountryName
-                    });
-                    marker.addListener('click', function () {
-                        infoWindow.open(map, marker);
-                    });
-                });
-            });
-        })
-        .catch(function (error) {
-            console.error("Hay un error aqui: " +  error.message);
+  fetch("https://sistemasgeo.com/2022/fetch/paises.json")
+    .then((response) => {
+      response.json().then((datos) => {
+        const map = new google.maps.Map(document.getElementById("map"), propiedades);
+        console.log(datos);
+        datos.forEach((marcador) => {
+          console.log(marcador);
+          var informacion = "<strong>País: </strong>" + marcador.CountryName;
+          var infoWindow = new google.maps.infoWindow({
+            content: informacion,
+          });
+          let marker = new google.maps.Marker({
+            map: map,
+            position: new google.maps.LatLng(marcador.CapitalLatitude, marcador.CapitalLongitude),
+            title: marcador.CountryName,
+          });
+          marker.addListener("click", function () {
+            infoWindow.open(map, marker);
+          });
         });
+      });
+    })
+    .catch(function (error) {
+      console.error("Hay un error aqui: " + error.message);
+    });
 }
-
-
