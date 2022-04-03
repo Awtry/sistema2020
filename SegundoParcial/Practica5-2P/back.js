ac86ee169251d8211486c5bc5511c52ee167ab12
Awtry <jorge_verah@outlook.es>
2022-04-03 04:22:29 -0500

    Ajuste lugares

--- SegundoParcial/Practica5-2P/back.js
+++ SegundoParcial/Practica5-2P/back.js
@@ -1,4 +1,4 @@
-
+/*
 var localidades = [
     {lat:  19.3327940, lng: -99.2125548, Nombre: "CDMX"}, //CDMX B
     {lat: 20.680933,  lng: -103.462491, Nombre: "Guadalajara"}, //GDL C
@@ -24,70 +24,128 @@ var localidades = [
     {lat: -21.441783, lng: 46.157604},//MADAGASCAR W
     {lat: 25.196866, lng: 55.263843}//DUBAI X
 ];
+*/
 
 var coordenadas = {
-    lat: 21.161728, lng: -101.686310
-}
+  lat: 21.161728,
+  lng: -101.68631,
+};
+
+var propiedades = {
+  center: coordenadas,
+  zoom: 3,
+};
+let map, popup, Popup;
 
 function iniciaMapa() {
-    var map = new google.maps.Map(document.getElementById("mapa"),
-        {
-            center: coordenadas,
-            zoom: 3,
-        }
-    );
+  var marcadores = [];
 
-    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var cuenta = 1;
 
-    var marcadores = [];
+  fetch("./Paises.json").then((response) => {
+    console.log(response);
+    response.json().then((lugares) => {
+      var map = new google.maps.Map(document.getElementById("mapa"), propiedades);
+      console.log(lugares);
 
-    var cuenta = 1;
+      lugares.forEach((marcador) => {
+        console.log(marcador);
+        popup = new Popup(new google.maps.LatLng(-33.866, 151.196), document.getElementById("content"));
+        popup.setMap(mapa);
+      });
+    });
+  });
+  /*
+  localidades.forEach((localidad) => {
+    console.log(localidad);
 
-    localidades.forEach(localidad => {
-        console.log(localidad);
-
-        let marcador = new google.maps.Marker({
-            map: map,
-            position: localidad,
-            label: labels[cuenta % labels.length]
-        });
-
-        var informacion = "<strong>País:</strong>" + localidad.Nombre;
-
-        var infowindow = new google.maps.InfoWindow({
-          content: informacion,
-        });
-
-        marcador.addListener("click", function() {
-            infowindow.open(map, marcador);
-        });
-
-        cuenta++;
-
-        marcadores.push(marcador);
+    let marcador = new google.maps.Marker({
+      map: map,
+      position: localidad,
     });
 
-    var markerCluster = new MarkerClusterer(map, marcadores, {
-        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
-        gridSize: 60,
-        zoomClick: true,
-        maxZoom: 10
+    var informacion = "<div class=" + "container-fluid";
+
+    var infowindow = new google.maps.InfoWindow({
+      content: informacion,
     });
 
-    document.getElementById("btnroadmap").addEventListener("click", function(){
-        map.setMapTypeId('terrain');
+    marcador.addListener("mouseover", function () {
+      infowindow.open(map, marcador);
     });
 
-    document.getElementById("btnsatellite").addEventListener("click", function(){
-        map.setMapTypeId('satellite');
+    cuenta++;
 
-    });
+    marcadores.push(marcador);
+  });
+*/
+  class Popup extends google.maps.OverlayView {
+    position;
+    containerDiv;
+    constructor(position, content) {
+      super();
+      this.position = position;
+      content.classList.add("popup-bubble");
 
-    document.getElementById("btnhybrid").addEventListener("click", function(){
-        map.setMapTypeId('hybrid');
-    });
+      // This zero-height div is positioned at the bottom of the bubble.
+      const bubbleAnchor = document.createElement("div");
 
-    document.getElementById("btnterrain").addEventListener("click", function(){
-        map.setMapTypeId('terrain');
-    });
+      bubbleAnchor.classList.add("popup-bubble-anchor");
+      bubbleAnchor.appendChild(content);
+      // This zero-height div is positioned at the bottom of the tip.
+      this.containerDiv = document.createElement("div");
+      this.containerDiv.classList.add("popup-container");
+      this.containerDiv.appendChild(bubbleAnchor);
+      // Optionally stop clicks, etc., from bubbling up to the map.
+      Popup.preventMapHitsAndGesturesFrom(this.containerDiv);
+    }
+    /** Called when the popup is added to the map. */
+    onAdd() {
+      this.getPanes().floatPane.appendChild(this.containerDiv);
+    }
+    /** Called when the popup is removed from the map. */
+    onRemove() {
+      if (this.containerDiv.parentElement) {
+        this.containerDiv.parentElement.removeChild(this.containerDiv);
+      }
+    }
+    /** Called each frame when the popup needs to draw itself. */
+    draw() {
+      const divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
+      // Hide the popup when it is far out of view.
+      const display = Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ? "block" : "none";
+
+      if (display === "block") {
+        this.containerDiv.style.left = divPosition.x + "px";
+        this.containerDiv.style.top = divPosition.y + "px";
+      }
+
+      if (this.containerDiv.style.display !== display) {
+        this.containerDiv.style.display = display;
+      }
+    }
+  }
+
+  var markerCluster = new MarkerClusterer(map, marcadores, {
+    imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    gridSize: 60,
+    zoomClick: true,
+    maxZoom: 10,
+  });
+
+  document.getElementById("btnroadmap").addEventListener("click", function () {
+    map.setMapTypeId("terrain");
+  });
+
+  document.getElementById("btnsatellite").addEventListener("click", function () {
+    map.setMapTypeId("satellite");
+  });
+
+  document.getElementById("btnhybrid").addEventListener("click", function () {
+    map.setMapTypeId("hybrid");
+  });
+
+  document.getElementById("btnterrain").addEventListener("click", function () {
+    map.setMapTypeId("terrain");
+  });
 }
