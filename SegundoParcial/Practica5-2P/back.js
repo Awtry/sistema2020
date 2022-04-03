3e043f71294b51d3aa9a81d10055fad539810091
Awtry <jorge_verah@outlook.es>
2022-04-03 12:30:10 -0500

    Correcciones

--- SegundoParcial/Practica5-2P/back.js
+++ SegundoParcial/Practica5-2P/back.js
@@ -1,30 +1,4 @@
-/*
-var localidades = [
-    {lat:  19.3327940, lng: -99.2125548, Nombre: "CDMX"}, //CDMX B
-    {lat: 20.680933,  lng: -103.462491, Nombre: "Guadalajara"}, //GDL C
-    {lat:  21.161728, lng: -101.686310, Nombre: "León"}, //LEON D
-    {lat:  41.4981238 , lng: 2.1567566, Nombre: "España "}, //ESPAÑA E
-    {lat: 25.689432, lng:  -100.317024, Nombre: "Monterrey"}, //MONTERREY F 
-    {lat: 41.890540, lng: 12.493813, Nombre: "Italia"},//ITALIA G
-    {lat: 36.161218, lng: -115.146309, Nombre: "Las Vegas"},// LAS VEGAS H
-    {lat: -22.904173, lng: -43.172127, Nombre: "Rio de Janeiro"},//RIO DE JANEIRO I
-    {lat: -34.616385, lng: -58.381344, Nombre: "Argentina"},// ARGENTINA J
-    {lat: 35.683730, lng: 139.767327}, //TOKYO K
-    {lat: 55.756816, lng: 37.617790},//RUSIA L
-    {lat: 31.229804, lng: 121.466993},//CHINA M
-    {lat: 40.766467, lng: -73.971459},//NUEVA YORK N
-    {lat: 64.803980, lng: -150.929412},//ALASKA O
-    {lat: 49.280711, lng: -123.117707},//VANCOUVER P
-    {lat: 64.146055, lng: -21.944433},//ISLANDIA Q
-    {lat: 59.913594, lng: 10.753161},//NORGUEGA R
-    {lat: 48.858787, lng: 2.292927},//FRANCIA S
-    {lat: 21.160806, lng: -86.853355},//CANCUN T
-    {lat: 32.771877, lng: -96.796294},//DALLAS U
-    {lat: 21.305364, lng: -157.858254},//HAWAII V
-    {lat: -21.441783, lng: 46.157604},//MADAGASCAR W
-    {lat: 25.196866, lng: 55.263843}//DUBAI X
-];
-*/
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
 
 var coordenadas = {
   lat: 21.161728,
@@ -73,7 +47,7 @@ function iniciaMapa() {
     });
   });
 
-  new MarkerClusterer(marcadores, map, {
+  const markerCluster = new MarkerClusterer(marcadores, map, {
     imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
     //gridSize: 60,
     //zoomClick: true,
