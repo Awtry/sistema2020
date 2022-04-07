cdbbc53ed40c16e359ba2ee42f1b98f5ab486e24
Awtry <jorge_verah@outlook.es>
2022-04-07 17:54:18 -0500

    faltante practica 8

--- SegundoParcial/Practica8-2P/back.js
+++ SegundoParcial/Practica8-2P/back.js
@@ -11,22 +11,26 @@ var coordenadas = {
 
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-
+  const idioma = urlParams.get("idioma");
   console.log(urlParams);
-  
+
+  var script = document.createElement("script");
+  script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCMrQgga-C5zCuZLTVk2MPVzX7naqKZXZU&callback=iniciarMapa&language=${idioma}`;
+
+  document.head.appendChild(script);
+
   var map;
   
   function iniciarMapa() {
-    var markers = [];
-    var separador = [];
-    var cuenta = 0;
-    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  
+    
+    map = new google.maps.Map(document.getElementById("mapa"), propiedades);
+
+
     fetch("./Paises.json").then((response) => {
       debugger;
       console.log(response);
       response.json().then((lugares) => {
-        map = new google.maps.Map(document.getElementById("mapa"), propiedades);
+        //map = new google.maps.Map(document.getElementById("mapa"), propiedades);
         console.log(lugares);
   
         lugares.forEach((lugar) => {
