647452cc5a2f7269c36035640bf2de1c54fed2d3
Awtry <jorge_verah@outlook.es>
2022-04-07 17:56:24 -0500

    Ajuste

--- SegundoParcial/Practica8-2P/back.js
+++ SegundoParcial/Practica8-2P/back.js
@@ -24,44 +24,5 @@ var coordenadas = {
   function iniciarMapa() {
     
     map = new google.maps.Map(document.getElementById("mapa"), propiedades);
-
-
-    fetch("./Paises.json").then((response) => {
-      debugger;
-      console.log(response);
-      response.json().then((lugares) => {
-        //map = new google.maps.Map(document.getElementById("mapa"), propiedades);
-        console.log(lugares);
-  
-        lugares.forEach((lugar) => {
-          console.log(lugar);
-  
-         
-          var infoWindowContent = '<div class="info_content">' + "<h5><striong> Nombre: </strong></h5>" + "<p>" + lugar.NombreLugar + "</p> <br>" + "<h5><striong> Capital: </strong></h5>" + "<p>" + lugar.Capital + "</p> <br>" + "<h5><striong> Habitantes: </strong></h5>" + "<p>" + lugar.Habitantes + "</p> <br>" + "<h5><striong> País: </strong></h5>" + "<p>" + lugar.Pais + "</p> <br>" + "</div>";
-          var infowindow = new google.maps.InfoWindow({
-            content: infoWindowContent,
-          });
-  
-          debugger;
-          separador.push({lat: lugar.Latitud, lng: lugar.Longitud})
-  
-          debugger;
-          var marker = new google.maps.Marker({
-            map: map,
-            position: new google.maps.LatLng(lugar.Latitud, lugar.Longitud),
-            label: labels[cuenta % labels.length]
-          });
-  
-          marker.addListener("click", function () {
-            infowindow.open(map, marker);
-          });
-          
-          cuenta++;
-          debugger;
-          markers.push(marker);
-          console.log("Marcador: " + cuenta + " , valor" + markers);
-        });
-      });
-    });
   
 }
