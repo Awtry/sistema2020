d72e323ec8016365d161d2de1685c13a0faf36f7
Awtry <jorge_verah@outlook.es>
2022-04-07 21:21:05 -0500

    Ajustes milagrosos

--- SegundoParcial/Practica9-2P/back.js
+++ SegundoParcial/Practica9-2P/back.js
@@ -1,6 +1,6 @@
 function iniciarMapa() {
 
- 
+
 
   var coordenadas = {
     lat: 21.152639,
@@ -18,184 +18,184 @@ function iniciarMapa() {
   var styledMap = new google.maps.StyledMapType([
     [
       {
-        "elementType": "geometry",
-        "stylers": [
+        elementType: 'geometry',
+        stylers: [
           {
             "color": "#212121"
           }
         ]
       },
       {
-        "elementType": "labels.icon",
-        "stylers": [
+        elementType: "labels.icon",
+        stylers: [
           {
             "visibility": "off"
           }
         ]
       },
       {
-        "elementType": "labels.text.fill",
-        "stylers": [
+        elementType: 'labels.text.fill',
+        stylers: [
           {
             "color": "#757575"
           }
         ]
       },
       {
-        "elementType": "labels.text.stroke",
-        "stylers": [
+        elementType: 'labels.text.stroke',
+        stylers: [
           {
             "color": "#212121"
           }
         ]
       },
       {
-        "featureType": "administrative",
-        "elementType": "geometry",
-        "stylers": [
+        featureType: 'administrative',
+        elementType: 'geometry',
+        stylers: [
           {
             "color": "#757575"
           }
         ]
       },
       {
-        "featureType": "administrative.country",
-        "elementType": "labels.text.fill",
-        "stylers": [
+        featureType: 'administrative.country',
+        elementType: 'labels.text.fill',
+        stylers: [
           {
             "color": "#c33737"
           }
         ]
       },
       {
-        "featureType": "administrative.land_parcel",
-        "stylers": [
+        featureType: 'administrative.land_parcel',
+        stylers: [
           {
             "visibility": "off"
           }
         ]
       },
       {
-        "featureType": "administrative.locality",
-        "elementType": "labels.text.fill",
-        "stylers": [
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [
           {
             "color": "#bdbdbd"
           }
         ]
       },
       {
-        "featureType": "poi",
-        "elementType": "labels.text.fill",
-        "stylers": [
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [
           {
             "color": "#757575"
           }
         ]
       },
       {
-        "featureType": "poi.park",
-        "elementType": "geometry",
-        "stylers": [
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [
           {
             "color": "#181818"
           }
         ]
       },
       {
-        "featureType": "poi.park",
-        "elementType": "labels.text.fill",
-        "stylers": [
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [
           {
             "color": "#616161"
           }
         ]
       },
       {
-        "featureType": "poi.park",
-        "elementType": "labels.text.stroke",
-        "stylers": [
+        featureType: 'poi.park',
+        elementType: 'labels.text.stroke',
+        stylers: [
           {
             "color": "#1b1b1b"
           }
         ]
       },
       {
-        "featureType": "road",
-        "elementType": "geometry.fill",
-        "stylers": [
+        featureType: 'road',
+        elementType: 'geometry.fill',
+        stylers: [
           {
             "color": "#84c238"
           }
         ]
       },
       {
-        "featureType": "road",
-        "elementType": "labels.text.fill",
-        "stylers": [
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [
           {
             "color": "#8a8a8a"
           }
         ]
       },
       {
-        "featureType": "road.arterial",
-        "elementType": "geometry",
-        "stylers": [
+        featureType: 'road.arterial',
+        elementType: 'geometry',
+        stylers: [
           {
             "color": "#cf4444"
           }
         ]
       },
       {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [
           {
             "color": "#78c4ce"
           }
         ]
       },
       {
-        "featureType": "road.highway.controlled_access",
-        "elementType": "geometry",
-        "stylers": [
+        featureType: 'road.highway.controlled_access',
+        elementType: 'geometry',
+        stylers: [
           {
             "color": "#4e4e4e"
           }
         ]
       },
       {
-        "featureType": "road.local",
-        "elementType": "labels.text.fill",
-        "stylers": [
+        featureType: 'road.local',
+        elementType: 'labels.text.fill',
+        stylers: [
           {
             "color": "#c8e774"
           }
         ]
       },
       {
-        "featureType": "transit",
-        "elementType": "labels.text.fill",
-        "stylers": [
+        featureType: 'transit',
+        elementType: 'labels.text.fill',
+        stylers: [
           {
             "color": "#757575"
           }
         ]
       },
       {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [
           {
             "color": "#000000"
           }
         ]
       },
       {
-        "featureType": "water",
-        "elementType": "labels.text.fill",
-        "stylers": [
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [
           {
             "color": "#3d3d3d"
           }
@@ -204,7 +204,7 @@ function iniciarMapa() {
     ]
   ], { name: "Mapa oscuro" });
 
-  
+
   var map = new google.maps.Map(document.getElementById("mapa"), propiedades);
 
   map.mapTypes.set("style_map", styledMap);
